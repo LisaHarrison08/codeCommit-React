d20a0471cd61db67f9c27a99ed4f2bea2f818239
Lisa Harrison <lmharrison0810@gmail.com>
2022-03-08 18:45:26 -0500

    About component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Info from './components/Info';
+import About from './components/About';
 
 function App() {
   return (
-    <div>
+    <React.Fragment>
       < Header />
       <Info />
-    </div>
+      <About />
+    </React.Fragment>
   )
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,37 @@
+import React from "react"
+import inspire from "../assets/inspire.png"
+import { Container, Col, Row } from "react-bootstrap"
+import './About.css'
+
+function About() {
+    return (
+        <Container fluid>
+            <Row>
+                <Col xs={12} md={6}>
+                    <img
+                        className="image"
+                        src={inspire}
+                        alt="Person sat at a computer"
+                    />
+                </Col>
+                <Col xs={12} md={6}>
+                    <h2 id="about">About Us</h2>
+                    <h4>Code Commit is an organization that...</h4>
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam
+                            rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+                            explicabo.
+                        </p>
+                        <p>
+                            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
+                            magnidolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia
+                            dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
+                            dolore magnam aliquam quaerat voluptatem.
+                        </p>
+                </Col>
+            </Row>
+
+        </Container>
+    )
+}
+
+export default About;
